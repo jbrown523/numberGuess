b71bfb1dbbef1998602ec5e0b988b081e96f0bc6
Jalen Brown <jalenbrown523@gmail.com>
2022-06-25 13:58:46 -0500

    Simple Number Guesser Console App - Complete

--- Program.cs
+++ Program.cs
@@ -2,65 +2,88 @@
 
 // Uses top-level statements, no Main method written explicitly
 
-// Variables
-string appName = "Number Guesser";
-string appVersion = "version-1.0";
-string appAuthor = "Jalen";
+getAppInfo();
 
-// Change text color
-Console.ForegroundColor = ConsoleColor.Yellow;
+getUserInfo();
 
-// App info
-Console.WriteLine("Welcome to {0}: {1}, created by {2}", appName, appVersion, appAuthor);
-
-// Reset text color
-Console.ResetColor();
-
-// User info
-Console.Write("Name: ");
-string userName = Console.ReadLine();
-Console.WriteLine("Hey {0}, let's play a game.", userName);
-
-
-// init Correct answer
-int answer = 9;
-
-//init user guess
-int guess = 0;
-
-
-Console.Write("Guess a number from 1-10: ");
-
-while (guess != answer)
+// Play game until user closes app
+while (true)
 {
-    // User input
-    string input = Console.ReadLine();
+    // init random answer
+    Random randomNum = new Random();
+    int answer = randomNum.Next(10);
 
-    // Check user input is number
-    if(!int.TryParse(input, out guess))
+    // init user guess
+    int guess = 0;
+
+
+    Console.Write("Guess a number from 1-10: ");
+
+    while (guess != answer)
     {
-        Console.ForegroundColor=ConsoleColor.Red;
-        Console.Write("Invalid input, please enter a nubmer: ");
-        Console.ResetColor();
+        // User input
+        string input = Console.ReadLine();
 
-        // Keep going
+        // Input must be a number
+        if (!int.TryParse(input, out guess))
+        {
+            printMessege(ConsoleColor.Red, "Invalid entry, please enter a nubmer: ");
+
+            // keep asking for guesses
+            continue;
+        }
+
+        // Parese to int
+        guess = Convert.ToInt32(input);
+
+        // Incorrect guess messege
+        if (guess != answer) { printMessege(ConsoleColor.Red, "Incorrect, please try again: "); }
+    }
+    // User guesses correclty, succes messege
+    printMessege(ConsoleColor.Yellow, "Correct! Nice guessing! ");
+
+    // Play again
+    Console.WriteLine("Would you like to play again? {Y or N}. Any other input exits the game.. ");
+
+    string continueAnswer = Console.ReadLine().ToUpper();
+
+    if (continueAnswer == "Y") {
         continue;
-    }
-
-    // Parese to int
-    guess = Convert.ToInt32(input);
-
-    if (guess != answer)
-    {
-        Console.ForegroundColor= ConsoleColor.Red;
-
-        Console.WriteLine("Incorrect guess, try again");
-
-        Console.ResetColor();
-
-    }
+    } else if (continueAnswer == "N") {
+        return;
+    } else { 
+        return; }
 }
-// User guesses correclty, succes messege
-Console.ForegroundColor = ConsoleColor.Yellow;
-Console.WriteLine("Correct! The answer is {0}, nice guessing!", answer);
-Console.ResetColor();
+
+static void getAppInfo()
+{
+    // Variables
+    string appName = "Number Guesser";
+    string appVersion = "version-1.0";
+    string appAuthor = "Jalen";
+
+    // Change text color
+    Console.ForegroundColor = ConsoleColor.Green;
+
+    // App info
+    Console.WriteLine("Welcome to {0}: {1}, created by {2}", appName, appVersion, appAuthor);
+
+    // Reset text color
+    Console.ResetColor();
+}
+
+// User info & greet
+static void getUserInfo()
+{
+    Console.Write("Name: ");
+    string userName = Console.ReadLine();
+    Console.WriteLine("Hey {0}, let's play a game.", userName);
+}
+
+// Output messege
+static void printMessege(ConsoleColor color, string messege)
+{
+    Console.ForegroundColor = color;
+    Console.Write(messege);
+    Console.ResetColor();
+}
